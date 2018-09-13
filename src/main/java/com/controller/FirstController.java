a3659887c559dae434cfc86ab062b706fe97d0a6
Administrator <1464090037@qq.com>
2018-09-13 20:23:32 +0800

    ssm半成品

--- src/main/java/com/controller/FirstController.java
+++ src/main/java/com/controller/FirstController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/")
 public class FirstController {
-    //ResponseBody 以字符串响应
+    //ResponseBody 以字符串响应 test connection
     @ResponseBody
     @RequestMapping("/hello.do")
     public String hello(){
